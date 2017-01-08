54f3eda22609a5018fe0e20687cd83901b6ad281
hannemann <hannemann666@gmx.net>
2017-01-08 21:42:36 +0100

    implement toggle shuffle mode

--- squeeze-favourite.py
+++ squeeze-favourite.py
@@ -49,7 +49,6 @@ class SqueezeFavorite(object):
             data = self.socket.recv(4096)
             if data or time.time() - start > 10:
                 break
-            print time.time() - start
         if data:
             self.parse_players(data)
 
@@ -61,9 +60,21 @@ class SqueezeFavorite(object):
             data = self.socket.recv(4096)
             if data or time.time() - start > 10:
                 break
-            print time.time() - start
         if data:
             self.parse_favorites(data)
+
+    def shuffle_playlist(self, player_name):
+        payload = "{} playlist shuffle ?\n".format(self.players[player_name])
+        self.socket.send(payload)
+        start = time.time()
+        while 1:
+            data = self.socket.recv(4096)
+            if data or time.time() - start > 10:
+                break
+        if data:
+            shuffled = int(data.split(' ')[-1:][0]) > 0
+            payload = "{} playlist shuffle {}\n".format(self.players[player_name], 0 if shuffled is True else 1)
+            self.socket.send(payload)
 
     def parse_players(self, data):
         data = data.split(' ')[5:]
@@ -109,42 +120,56 @@ def usage():
     print '\t\t-p\tPort (default 9090)'
     print '\t\t-r\tPlayer name'
     print '\t\t-f\tFavorite name'
+    print '\t\t-t\ttoggle shuffle'
     print '\n'
 
 
 if __name__ == "__main__":
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "s:p:r:f:hl", ["help"])
+        opts, args = getopt.getopt(sys.argv[1:], "s:p:r:f:hlt", ["help"])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
 
-    get_list = False
     server = None
     port = None
     player = None
     favorite = None
+    get_list = False
+    shuffle = False
     for opt, arg in opts:
         if opt in ('-h', '--help'):
             usage()
             sys.exit(0)
-        if opt == '-l':
-            get_list = True
         if opt == '-s':
             server = arg
         if opt == '-p':
-            port = int(arg)
+            try:
+                port = int(arg)
+            except ValueError:
+                usage()
+                sys.exit(1)
         if opt == '-r':
             player = arg
         if opt == '-f':
             favorite = arg
+        if opt == '-l':
+            get_list = True
+        if opt == '-t':
+            shuffle = True
+
+    if server is None:
+        usage()
+        sys.exit(2)
 
     with SqueezeFavorite(server, port) as squeeze:
         if get_list is True:
             squeeze.print_list()
         elif player and favorite:
             squeeze.start_favorite(player, favorite)
+        elif player and shuffle is not None:
+            squeeze.shuffle_playlist(player)
         else:
             usage()
             sys.exit(2)
